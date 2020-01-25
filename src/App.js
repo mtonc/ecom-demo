a22abf9c96fe5f8b8602e79b66a8c80027e8c3cc
mtonc <m.tonchick@gmail.com>
2020-01-24 20:07:06 -0600

    Split homepage into smaller components

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import HomePage from './pages/Homepage/Homepage';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+     <HomePage></HomePage>
+    </div>
+  );
+}
+
+export default App;
