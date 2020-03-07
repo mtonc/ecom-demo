c9d3a50afec75509e38b53a852ebbca1194c1563
mtonc <m.tonchick@gmail.com>
2020-03-07 14:32:07 -0600

    Added email password signup

--- src/components/SignUp/SignUp.jsx
+++ src/components/SignUp/SignUp.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
-import fromInput from '../FormInput/FormInput'
+import FormInput from '../FormInput/FormInput'
 import Button from '../Button/Button'
 import {auth, createUserProfileDocument} from '../../firebase/firebase.utils'
 
 import './signup.scss'
-import FormInput from '../FormInput/FormInput'
+
 
 class SignUp extends React.Component {
 	constructor() {
@@ -16,6 +16,36 @@ class SignUp extends React.Component {
 			password: '',
 			confirmPassword: ''
 		}
+	}
+
+	handleSubmit = async event => {
+		event.preventDefault();
+		const {displayName, email, password, confirmPassword} = this.state
+
+		if (password !== confirmPassword) {
+			alert("Passwords do not match!");
+		}
+
+		try {
+			const { user } = await auth.createUserWithEmailAndPassword(email, password)
+
+			await createUserProfileDocument(user, {displayName})
+
+			this.setState({
+				displayName: '',
+				email: '',
+				password: '',
+				confirmPassword: ''
+			})
+		} catch (error) {
+			console.error(error)
+		}
+	}
+
+	handleChange = event => {
+		const { name, value } = event.target
+
+		this.setState({[name]: value})
 	}
 
 	render() {
@@ -36,7 +66,7 @@ class SignUp extends React.Component {
 					<FormInput
 						type="email"
 						name="email"
-						value={displayName}
+						value={email}
 						onChange={this.handleChange}
 						label="Email"
 						required
@@ -57,8 +87,11 @@ class SignUp extends React.Component {
 						label="Display Name"
 						required
 					/>
+					<Button type='submit'>SIGN UP</Button>
 				</form>
 			</div>
 		)
 	}
 }
+
+export default SignUp
--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,11 +1,13 @@
-import React from 'react';
-import SignIn from '../../components/SignIn/SignIn';
-import './login.scss';
+import React from 'react'
+import SignIn from '../../components/SignIn/SignIn'
+import SignUp from '../../components/SignUp/SignUp'
+import './login.scss'
 
 const Login = () => (
-	<div className="login">
+	<div className='login'>
 		<SignIn/>
+		<SignUp/>
 	</div>
 )
 
-export default Login;
+export default Login
